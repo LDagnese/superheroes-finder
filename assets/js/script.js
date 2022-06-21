d1748f3b07beab84cc6b67b98cdb28ca6a8f5dff
loudagnese <louis.dagnese@gmail.com>
2022-06-20 18:06:19 -0700

    initial js to test out Marvel API permission from domain

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,26 @@
 // API handler for comicvine
 
 // element creation for MarvelAPI
+var getSuperHero = function () {
+    var apiUrl = "https://gateway.marvel.com:443/v1/public/characters/1017577?apikey=9878fa368575f3fd08db8e28234824f8"
+
+    // make a request
+    fetch(apiUrl)
+        .then(function(response){
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(data) {
+                    console.log(data);
+    
+                });
+            } else {
+                console.log('Error:Data not retrieved')
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+};
 
 // element creation for comicvine API
 
